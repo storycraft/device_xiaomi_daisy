7a1e6b7d3535c0515761b083311acc4d9c2a8407
pawelik001 <pawelik001@gmail.com>
2019-11-05 16:47:43 +0100

    temp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,5 +55,8 @@ VENDOR_SECURITY_PATCH := 2019-05-05
 # Sepolicy
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/daisy/BoardConfigVendor.mk
