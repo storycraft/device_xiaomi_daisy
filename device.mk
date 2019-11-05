2a169bae2b24832347f3ba29e47660dc49f088f4
pawelik001 <pawelik001@gmail.com>
2019-11-05 18:27:57 +0100

    temp

--- device.mk
+++ device.mk
@@ -40,17 +40,16 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.msm8953.recovery
+
+PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
-    bootctrl.msm8953.recovery \
+    bootctrl.msm8953 \
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.msm8953 \
-    libcutils \
-    libz
 
 # Inherit from msm8953-common
 $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
