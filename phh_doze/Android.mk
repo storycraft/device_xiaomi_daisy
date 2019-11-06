57549b8fb39703f72bdcda88a84f03cfe979aff8
acras01 <acras1@gmail.com>
2019-11-06 13:01:22 +0200

    Add phh_doze app

---
+++ phh_doze/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := phh_Doze
+LOCAL_SRC_FILES := app.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_BUILT_MODULE_STEM := package.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := XiaomiParts XiaomiDoze
+
+include $(BUILD_PREBUILT)
