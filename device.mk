1c1e156c6755e9212543a6b06a6fad273f219241
storycraft <storycraft@storyboard.ml>
2020-02-26 19:12:39 +0900

    try activating touch hal again

--- device.mk
+++ device.mk
@@ -91,6 +91,10 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+	
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_8953
 
 # Trust HAL
 PRODUCT_PACKAGES += \
