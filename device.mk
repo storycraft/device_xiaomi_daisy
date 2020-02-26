1a98bf028ccb72eaabb17cfcbfbb48e528ccd106
storycraft <storycraft@storyboard.ml>
2020-02-26 22:48:39 +0900

    move trust hal to common

--- device.mk
+++ device.mk
@@ -96,10 +96,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_8953
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
